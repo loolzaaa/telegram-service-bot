c8ea5fe04b2fd51bb5aacddfad9c58488d78a7ec
loolzaaa <loolzaaa@gmail.com>
2022-02-13 02:22:52 +0500

    Update CircleCIResultCommand

--- service-bot-core/src/main/java/ru/loolzaaa/telegram/servicebot/core/commands/circleci/CircleCIResultCommand.java
+++ service-bot-core/src/main/java/ru/loolzaaa/telegram/servicebot/core/commands/circleci/CircleCIResultCommand.java
@@ -5,7 +5,11 @@ import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import ru.loolzaaa.telegram.servicebot.core.bot.pojo.Configuration;
+import ru.loolzaaa.telegram.servicebot.core.circleci.pojo.Project;
 import ru.loolzaaa.telegram.servicebot.core.commands.CommonCommand;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CircleCIResultCommand extends CommonCommand {
 
@@ -18,16 +22,36 @@ public class CircleCIResultCommand extends CommonCommand {
         if (arguments.length >= 6) {
             final String type = arguments[0];
             final String time = arguments[1];
-            final String projectId = arguments[2];
-            final String projectName = arguments[3];
+            final String projectName = arguments[2];
+            String projectSlug = arguments[3];
             final String workflowName = arguments[4];
             final String workflowStatus = arguments[5];
+            if ("workflow-completed".equals(type)) {
+                if (projectSlug.toLowerCase().startsWith("gh")) projectSlug = "github" + projectSlug.substring(2);
+                if (projectSlug.toLowerCase().startsWith("bb")) projectSlug = "bitbucket" + projectSlug.substring(2);
 
-            SendMessage message = new SendMessage();
-            message.setChatId("@circleci_results");
-            message.setText(String.join(" ", arguments));
-
-            sendAnswer(absSender, message);
+                final String conditionSlug = projectSlug;
+                configuration.getUsers().stream()
+                        .filter(u -> {
+                            List<Project> circleCIProjects = u.getCircleCIProjects();
+                            if (circleCIProjects != null) {
+                                List<String> slugs = circleCIProjects.stream()
+                                        .map(project -> project.getSlug().toLowerCase())
+                                        .collect(Collectors.toList());
+                                return slugs.contains(conditionSlug.toLowerCase());
+                            } else {
+                                return false;
+                            }
+                        })
+                        .forEach(u -> {
+                            SendMessage message = new SendMessage();
+                            message.setChatId(u.getChatId().toString());
+                            message.setText(String.join(" ", arguments));
+                            sendAnswer(absSender, message);
+                        });
+            } else {
+                //TODO: add other type support
+            }
         }
     }
 }
