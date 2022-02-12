d34b2487a3e18580d66d9c6b44b3d029dacccddb
loolzaaa <loolzaaa@gmail.com>
2022-02-13 02:22:09 +0500

    Add new fields to User entity

--- service-bot-core/src/main/java/ru/loolzaaa/telegram/servicebot/core/bot/pojo/User.java
+++ service-bot-core/src/main/java/ru/loolzaaa/telegram/servicebot/core/bot/pojo/User.java
@@ -1,46 +1,24 @@
 package ru.loolzaaa.telegram.servicebot.core.bot.pojo;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import ru.loolzaaa.telegram.servicebot.core.circleci.pojo.Project;
+
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class User {
-
     private Long id;
-
+    private String firstName;
+    private String username;
+    private Long chatId;
     private LocalDateTime lastActivity;
-
     private List<TrackEntry> trackHistory;
-
-    public User() {
-    }
-
-    public User(Long id) {
-        this.id = id;
-        this.trackHistory = new ArrayList<>();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getLastActivity() {
-        return lastActivity;
-    }
-
-    public void setLastActivity(LocalDateTime lastActivity) {
-        this.lastActivity = lastActivity;
-    }
-
-    public List<TrackEntry> getTrackHistory() {
-        return trackHistory;
-    }
-
-    public void setTrackHistory(List<TrackEntry> trackHistory) {
-        this.trackHistory = trackHistory;
-    }
+    private List<Project> circleCIProjects;
 }
